fe3b69d6a1cbad3d1dbea89ce2778a8a1358a8f8
berguera <robin.berguerand@epfl.ch>
2018-12-19 23:27:56 +0100

    pip

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import scipy.sparse as sp
-from surprise import Dataset, Reader
+from scipy import optimize
+from surprise import Dataset, Reader,accuracy
 import numpy as np
 
 def load_data_surprise(path,threshold):
@@ -15,7 +15,59 @@ def load_data_surprise(path,threshold):
     
     return dataset, dataframe
 
+def transform(prediction):
+    pred=[p[3] for p in prediction]
+    return np.reshape(pred,(len(pred),1))
 
+def global_mean_pred(trainset,testset,predset):
+    mean=trainset.global_mean
+    return mean*np.ones([len(testset),1]),mean*np.ones([len(predset),1]),
+
+def user_item_func(dic,ele,mean):
+    if ele in dic:
+        return dic[ele]
+    else:
+        return mean
+
+def user_mean_pred(trainset,testset,predset):
+    mean=trainset.global_mean
+    trainset=trainset.build_testset()
+    trainset=pd.DataFrame(trainset, columns=['User', 'Item', 'Prediction'])
+    g = trainset.groupby('User')
+    user_mean  = g.mean()
+    dict_mean = user_mean.to_dict()['Prediction']
+    usermean_test=np.array([user_item_func(dict_mean,p[0],mean) for p in testset])
+    usermean_pred=np.array([user_item_func(dict_mean,p[0],mean) for p in predset])
+    usermean_test=np.reshape(usermean_test,(len(usermean_test),1))
+    usermean_pred=np.reshape(usermean_pred,(len(usermean_pred),1))
+    return usermean_test,usermean_pred
+
+def item_mean_pred(trainset,testset,predset):
+    mean = trainset.global_mean
+    trainset = trainset.build_testset()
+    trainset = pd.DataFrame(trainset, columns=['User', 'Item', 'Prediction'])
+    g = trainset.groupby('Item')
+    item_mean  = g.mean()
+    dict_mean = item_mean.to_dict()['Prediction']
+    itemmean_test = np.array([user_item_func(dict_mean,p[0],mean) for p in testset])
+    itemmean_pred = np.array([user_item_func(dict_mean,p[0],mean) for p in predset])
+    itemmean_test = np.reshape(itemmean_test,(len(itemmean_test),1))
+    itemmean_pred = np.reshape(itemmean_pred,(len(itemmean_pred),1))
+    return itemmean_test,itemmean_pred
+
+def train_model(algo,trainset,testset,predset):
+    print('Fit model...')
+    algo.fit(trainset)
+    print('Make predictions...')
+    pred_test = algo.test(testset)
+    pred_sub  = algo.test(predset)
+    return transform(pred_test),transform(pred_sub)
+    
+def coeff(pred_set,real_val):
+    l=np.shape(pred_set)[1]
+    x0=np.ones([1,l])*1/l
+    opt=optimize.minimize(calculate_poud_mse,x0,args=(real_val,pred_set.T),method='SLSQP')
+    return opt['x']
 def load_data_matrix(path):
     def deal_line(line):
         pos, rating = line.split(',')
@@ -40,11 +92,7 @@ def load_data_matrix(path):
     for row, col, rating in data:
         ratings[row - 1, col - 1] = rating
     return ratings
-def rounding(i):
-    return round(i)
-def calculate_rmse_pred(prediction):
-    t = np.array([p[2]- rounding(p[3]) for p in prediction])
-    return np.sqrt((1.0 * t.dot(t.T))/len(prediction))
+        
 def n_error(prediction):
     n=0
     g=0
@@ -62,5 +110,21 @@ def calculate_r(prediction):
     return n
 def calculate_mse(real_label, prediction):
     """calculate MSE."""
+    t = real_label - prediction
+    return 1.0 * t.dot(t.T)
+def calculate_mse_round(real_label, prediction):
+    """calculate MSE."""
     t = real_label - np.round(prediction)
     return 1.0 * t.dot(t.T)
+def calculate_poud_mse(poud,real_label,prediction):
+    prediction= poud @ prediction
+    t = real_label - prediction
+    return 1.0 * t.dot(t.T)    
+def calculate_poud_rmse_round(poud,real_label,prediction):
+    prediction= poud @ prediction
+    t = real_label - np.round(prediction)
+    return np.sqrt((1/len(real_label))* t.dot(t.T)) 
+def calculate_rmse(real_label, prediction):
+    """calculate MSE."""
+    mse=calculate_mse(real_label, prediction)
+    return np.sqrt(mse/len(real_label))
